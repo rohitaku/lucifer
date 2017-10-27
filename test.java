e4e8ce7a95fc2b7912e89cebf8663c91eb076fdf
Nayak <priyanshu.nayak@capgemini.com>
2017-10-27 15:22:02 +0530

    yo

---
+++ test.java
@@ -0,0 +1,18 @@
+package com.cg.fs.hsbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class test
+{
+String name="psycoder123";
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+	public String greetMe(){
+	
+	return "Welcome:"+name;
+}
+}
